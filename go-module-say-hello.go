11bc98134c82b0c20b5d30ff91ec5e03854c46ea
fahrizalm <fahrizalmaulana.fm@gmail.com>
2022-06-08 10:16:38 +0700

    return value berubah ke v2

--- go-module-say-hello.go
+++ go-module-say-hello.go
@@ -1,5 +1,22 @@
 package module_say_hello
 
+/*
+	1. buat nama package modul sebagai nama dependency ketika diakase pd import
+	2. buat go mod
+	   dgn perintah: go mod init disertai url github project atau nama modules
+	3. lakukan perintah simpan project/module di repository (github)
+	   - git init
+	   - git add .
+	   - git commit -m "deskripsi"
+	   - git add origin master disertai url github untuk menyimpan ke github
+	   - git push origin master
+	2. buat tag versi untuk merelease module berdasarkan versi dgn cara:
+	   - git tag v1.0.0
+	   - git push origin v1.0.0
+
+	3. buat file dgn function main untuk mencetak data dari function modules	
+*/
+
 func SayHello(nama string) string {
 	return "hello 1 " + nama
 }
